040cc394fa79ff4e93345e884700817a7262208f
Renata Carriero <Renata.Carriero@icubed.it>
2021-11-12 15:51:30 +0100

    Correzione Prova Agenti

---
+++ Agenti/IManagerAgenti.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenti
+{
+    interface IManagerAgenti
+    {
+        public List<Agente> GetAll();
+        public List<Agente> GetByAreaGeografica(string areaGeografica);
+        public List<Agente> GetByAnniDiServizio(int anniDiServizio);
+        public bool Add(Agente agente);
+
+        public List<string> GetAllAreeGeografiche();
+        public bool EsisteArea(string areaGeografica);
+        public Agente GetByCF(string cf);
+    }
+}
